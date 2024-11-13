f4d8c03e4e2a17b65687fe04c1d0cef5eb417e34
Anang Kurniawan <anangk97@gmail.com>
2024-11-13 23:12:57 +0700

    Initial Commit

---
+++ calculatorbillkuTests/calculatorbillkuTests.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorbillkuTests.swift
+//  calculatorbillkuTests
+//
+//  Created by Anang Kurniawan on 13/11/24.
+//
+
+import Testing
+@testable import calculatorbillku
+
+struct calculatorbillkuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
