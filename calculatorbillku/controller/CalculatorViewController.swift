c5f3b616251a9e5db064a4f2fe6ec7b25ef33304
Anang Kurniawan <anangk97@gmail.com>
2024-11-19 15:53:16 +0700

    feat: implement timChanged ibaction

--- calculatorbillku/controller/CalculatorViewController.swift
+++ calculatorbillku/controller/CalculatorViewController.swift
@@ -15,12 +15,53 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var buttonZeroPercent: UIButton!
     @IBOutlet weak var billTotalTextField: UITextField!
     
+    private var tip: Float = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resetPercentButtonSelected()
     }
 
     @IBAction func tipChanged(_ sender: UIButton) {
+        switch sender.titleLabel?.text {
+        case "0%":
+            setZeroPercentButtonSelected()
+            tip = 0.0
+        case "10%":
+            setTenPercentButtonSelected()
+            tip = 0.1
+        case "20%":
+            setTwentyPercentButtonSelected()
+            tip = 0.2
+        default:
+            resetPercentButtonSelected()
+            tip = 0.0
+        }
+        print(tip)
+    }
+    
+    private func setZeroPercentButtonSelected() {
+        buttonZeroPercent.backgroundColor = .black
+        buttonTenPercent.backgroundColor = nil
+        buttonTwentyPercent.backgroundColor = nil
+    }
+    
+    private func setTenPercentButtonSelected() {
+        buttonZeroPercent.backgroundColor = nil
+        buttonTenPercent.backgroundColor = .black
+        buttonTwentyPercent.backgroundColor = nil
+    }
+    
+    private func setTwentyPercentButtonSelected() {
+        buttonZeroPercent.backgroundColor = nil
+        buttonTenPercent.backgroundColor = nil
+        buttonTwentyPercent.backgroundColor = .black
+    }
+    
+    private func resetPercentButtonSelected() {
+        buttonZeroPercent.backgroundColor = nil
+        buttonTenPercent.backgroundColor = nil
+        buttonTwentyPercent.backgroundColor = nil
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
